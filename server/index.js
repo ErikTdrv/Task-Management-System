2de81e36b000def265bcef4e5f8a7341ab954bfc
Erik Todorov <eriktdrv@gmail.com>
2023-06-10 16:27:14 +0300

    Adding routes & dependancies

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const initDatabase = require('./configs/initDatabase');
+
+app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+const PORT = 7070 || process.env.PORT;
+app.listen(PORT, () => console.log(`Server listening on localhost:${PORT}`));
+initDatabase()
+    .then(() => console.log('Database working!'))
+    .catch((e) => console.log('Database Error: ' + e))
