09fb56302441135b55f91ea4d95932e7a8b58b58
Erik Todorov <eriktdrv@gmail.com>
2023-06-11 10:51:51 +0300

    Adding Router

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Register from './components/Authentication/Register';
 import Login from './components/Authentication/Login';
+import { Routes, Route } from 'react-router-dom';
+import InitialPage from './components/InitialPage/InitialPage';
 
 function App() {
   return (
-    <div className="App">
-      <Register />
-    </div>
+    <Routes>
+      <Route exact path="/" element={<InitialPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 }
 
